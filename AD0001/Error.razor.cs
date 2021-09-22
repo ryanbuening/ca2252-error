c72b042d25898df3760dbb86a555b56f38b56c63
Ryan Buening <ryan.buening@celinainsurance.com>
2021-09-22 15:16:41 -0400

    init

---
+++ AD0001/Error.razor.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace CA2252.AD0001;
+
+public partial class Error
+{
+	public EditContext EditContext { get; }
+
+	public Error()
+	{
+		EditContext = new EditContext(""); // CA2252 - AD0001 error
+	}
+}
